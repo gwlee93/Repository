60b9722f1300120c2a93693c7c7bfd80ba68231e
gwlee93 <pops103@gmail.com>
2024-03-19 22:16:13 +0900

    Initial Commit

---
+++ GrpcService/GrpcService/Commands/AddStudentCommand.cs
@@ -0,0 +1,7 @@
+﻿using LanguageExt;
+using MediatR;
+
+namespace GrpcService.Commands
+{
+    public record AddStudentCommand(StudentRequest request) : IRequest<Option<string>>;
+}
---
+++ GrpcService/GrpcService/Handlers/AddStudentHandler.cs
@@ -0,0 +1,31 @@
+﻿using GrpcService.Commands;
+using GrpcService.Entities;
+using GrpcService.Services;
+using LanguageExt;
+using MediatR;
+using StudentDTO = GrpcService.StudentDTO;
+
+namespace GrpcService.Handlers
+{
+    public class AddStudentHandler : IRequestHandler<AddStudentCommand, Option<string>>
+    {
+
+        private readonly RedisService _redisService;
+        public AddStudentHandler(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Option<string>> Handle(AddStudentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = Student.Create("1", "이건우", 32, "Math");            
+            await _redisService.EnqueueAsync(entity);
+            var result = await _redisService.DequeueAsync();            
+            if (result is null)
+                return Option<string>.None;
+           
+            return Option<string>.Some(result);
+
+        }     
+    }
+}
